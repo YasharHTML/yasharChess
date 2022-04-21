1f886688d99c1ae189b38258df8887f86d859472
Yashar <yashar220@hotmail.com>
2022-04-21 18:09:06 +0400

    newcommit with fixed bishop

--- public/script.js
+++ public/script.js
@@ -5,6 +5,19 @@ const hasPiece = (element) => {
     return element.classList.contains('piece');
 }
 
+// list of all coordinates
+const coordinates = [
+    'a1', 'a2', 'a3', 'a4', 'a5', 'a6', 'a7', 'a8',
+    'b1', 'b2', 'b3', 'b4', 'b5', 'b6', 'b7', 'b8',
+    'c1', 'c2', 'c3', 'c4', 'c5', 'c6', 'c7', 'c8',
+    'd1', 'd2', 'd3', 'd4', 'd5', 'd6', 'd7', 'd8',
+    'e1', 'e2', 'e3', 'e4', 'e5', 'e6', 'e7', 'e8',
+    'f1', 'f2', 'f3', 'f4', 'f5', 'f6', 'f7', 'f8',
+    'g1', 'g2', 'g3', 'g4', 'g5', 'g6', 'g7', 'g8',
+    'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'h7', 'h8'
+];
+
+
 socket.on("move_to", (data) => {
     console.log(data);
     document.getElementById(data.target).appendChild(document.getElementById(data.origin).children[0]);
@@ -79,10 +92,13 @@ document.addEventListener("click", (e) => {
         }
         if (e.target.classList.contains("bishop")) {
             checkBishopMove(e.target.parentElement.id).forEach(element => {
-                if (document.getElementById(element).classList.contains("w")) {
-                    document.getElementById(element).classList.add("possible");
-                } else {
-                    document.getElementById(element).classList.add("possible-b");
+                console.log("-------" + element + "-------")
+                if (coordinates.includes(element)) {
+                    if (document.getElementById(element).classList.contains("w")) {
+                        document.getElementById(element).classList.add("possible");
+                    } else {
+                        document.getElementById(element).classList.add("possible-b");
+                    }
                 }
             })
         }
